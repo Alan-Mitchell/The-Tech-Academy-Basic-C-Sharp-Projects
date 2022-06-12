699d18eb329de19475321cd1714075db3cd3e4a3
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-06-12 00:28:31 -0500

    submit

---
+++ BooleanLogicSubmissionAssignment/BooleanLogicSubmissionAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age? ");
+            int ageCheck = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? True or False ");
+            bool DUIstatus = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets have you had? ");
+            int SpeedingTickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            Console.ReadLine();
+            bool Qualified = ageCheck > 15 && DUIstatus == false && SpeedingTickets < 3;
+            Console.WriteLine(Qualified);
+            Console.ReadLine();
+
+
+        }
+    }
+}
