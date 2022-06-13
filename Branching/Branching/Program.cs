4af9eb6462305b9ed1b095d0f022102962a15dad
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-06-13 15:50:19 -0500

    UPDATE

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            String results = favNum == 12 ? "You have an awesome favorite Number" : "You do not have an awesome favorite number";
+
+            Console.WriteLine(results);
+            Console.ReadLine();
+            
+
+            //    int roomTemprature = 70;
+
+            //    Console.WriteLine("Hi, what is your name?: ");
+            //    string name = Console.ReadLine();
+
+            //    Console.WriteLine("Hi, " + name + ", what is the remprature where you are?");
+            //    int currentTemprature = Convert.ToInt32(Console.ReadLine());
+
+            //    if (currentTemprature == roomTemprature)
+            //    {
+            //        Console.WriteLine("it is exactly room temprature");
+
+            //    }
+            //    else if (currentTemprature > roomTemprature)
+            //    {
+            //        Console.WriteLine("It is warmer than room temprature.");
+
+            //    }
+            //    else if (roomTemprature > currentTemprature)
+            //    {
+            //        Console.WriteLine("Room temprature is warmenr than current temprature");
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("This will never run!");
+            //    }
+
+            Console.ReadLine();
+            //int currentTemprature = 80;
+            //int roomTemprature = 70;
+
+            //string comparisonResult = currentTemprature == roomTemprature ? "It is room temp" : "It is not room temp";
+
+            //Console.WriteLine(comparisonResult);
+            ////if(currentTemprature == roomTemprature)
+            ////{
+            ////    Console.WriteLine("It is exactly room temprature. ");
+
+            ////}
+            ////else if(currentTemprature > roomTemprature)
+            ////    {
+            ////    Console.WriteLine("It is warmer than room temprature");
+            ////}
+            ////else if (roomTemprature > currentTemprature)
+            ////{
+            ////    Console.WriteLine("Room temprature is  greater than current temprature");
+            ////}
+            ////else
+            ////{
+            ////    Console.WriteLine("It is not exactly room temprature.");
+               
+
+            ////}
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please Follow the instructions below");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter the weight of your package.");
+            int pkgWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (pkgWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.");
+                Console.ReadLine();
+            }
+            else if (pkgWeight < 50)
+            {
+                Console.WriteLine("What is the width of your package?");
+                int pkgwidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is your package height?");
+                int pkgheight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the length of your package?");
+                int pkglength = Convert.ToInt32(Console.ReadLine());
+                if (pkgwidth * pkgheight * pkglength > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+                else {
+                    int total = pkgwidth * pkgheight * pkglength * pkgWeight / 100;
+                    Console.WriteLine("Your total is $" + total);
+                    Console.ReadLine();
+                }
+
+            }
+            
+
+
+        }
+    }
+}
