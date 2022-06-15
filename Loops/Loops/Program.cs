248c5aa24a311b930c9d9d5ce6aefba00e7db68b
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-06-15 12:26:32 -0500

    update

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+            
+            while (!isGuessed)
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62, try again");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29, try again");
+                        Console.WriteLine("Guess a number");
+                         number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55, try again");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("That is Correct!");
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+
+            }
+                
+            {
+                
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This example below displays a while loop
+            int number = 76;
+            while(number < 80)
+                {
+                Console.WriteLine(number++);
+                Console.ReadLine();
+
+            }
+            do
+            {
+                Console.WriteLine(number--);
+                Console.ReadLine();
+            }
+            while (number > 70);
+
+
+        }
+
+    }
+}
