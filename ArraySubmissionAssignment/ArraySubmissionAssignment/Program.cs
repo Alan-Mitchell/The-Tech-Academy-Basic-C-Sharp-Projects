e4b9f43348492885395363444b2a601ef1f56b19
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-06-26 13:37:37 -0500

    update

---
+++ ArraySubmissionAssignment/ArraySubmissionAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Colors = { "Blue", "Green", "Red" };
+            Console.WriteLine("Please enter a number! ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if(input >2 )
+            {
+                Console.WriteLine("please choose a smaller number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("The color you chose was " + Colors[input]);
+                Console.ReadLine();
+            }
+        }
+    }
+}
