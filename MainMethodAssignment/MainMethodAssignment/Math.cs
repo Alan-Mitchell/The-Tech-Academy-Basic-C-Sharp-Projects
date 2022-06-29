f9bab385538a856a88b8771f58deab42d0cf5070
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-06-29 10:50:53 -0500

    update

---
+++ MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Math
+    {
+        public int MathMethod(int x, int y)
+        {
+            return x + y;
+        }
+    }
+   
+}
+
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math operation = new Math();
+            {
+                Console.WriteLine(operation.MathMethod(2, 3));
+                Console.ReadLine();
+            }
+            Math operationF = new Math();
+            {
+                Console.WriteLine(operationF.MathMethod( 2.2, 3.3));
+                Console.ReadLine();
+
+            }
+            Math operatonS = new Math();
+            {
+                Console.WriteLine(operatonS.MathMethod(1, 3));
+                Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
