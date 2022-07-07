6036d1a69a277d3517fa50cf5e8e976ebd96ddac
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-07-07 08:11:41 -0500

    update

---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/NewClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmissionAssignment
+{
+    class NewClass
+    {
+        
+        
+
+
+        public void Dvision()
+        {
+            int a = 2;
+            Console.WriteLine("Give a number to divide");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(b / a);
+            Console.ReadLine();
+           
+
+        }
+
+
+    }
+
+    public class x
+    {
+    }
+}
