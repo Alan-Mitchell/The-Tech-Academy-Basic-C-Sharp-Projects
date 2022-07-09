5b0b9f95816f31316a6bf78af67a69630422d9b1
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-07-08 21:52:42 -0500

    update

---
+++ MethodsAndObjectsSubmission/MethodsAndObjectsSubmission/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsSubmission
+{
+    class Program : Person
+    {
+        static void Main(string[] args)
+        {
+            Employee Sample = new Employee() {FirstName = "Sample", LastName = "Student" };
+            Person.SayName();
+        }
+    }
+}
