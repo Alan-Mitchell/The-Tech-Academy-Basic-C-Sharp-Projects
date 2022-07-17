fb43a6abbda529fb4902c2a402d4dbba4a7fe71d
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-07-17 12:11:06 -0500

    update

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //New instance of a datetime object
+            DateTime dateTime = new DateTime(2022, 7, 17, 12, 03, 34);
+            Console.WriteLine(dateTime);
+
+            Console.WriteLine("Please provide a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            DateTime dateTime1 = new DateTime(2022, 7, 17, 12 + x, 03, 34);
+            Console.WriteLine(dateTime1);
+
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Game.cs
+++ TwentyOne/TwentyOne/Game.cs
@@ -8,10 +8,13 @@ namespace TwentyOne
 {
     public abstract class Game
     {
-        public List<Player> Players { get; set; }
-        public string Name { get; set; }
+        private List<Player> _players = new List<Player>();
+        private Dictionary<Player, int> _bets = new Dictionary<Player, int>();
 
-        public string Dealer { get; set; }
+        public List<Player> Players { get { return _players; }  set { _players = value; } }
+        public string Name { get; set; }
+        public Dictionary<Player, int> Bets { get { return _bets; } set { _bets = value; } }
+
 
         public abstract void Play();
 
