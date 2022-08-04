65061a2fdb385c9ef8c3387f8287edf9c28a0f42
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-08-03 21:21:02 -0500

    update

--- ConstructorSubmission/ConstructorSubmission/Program.cs
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -16,19 +16,31 @@ public class ConstTest
         public const int C1 = 5;
         public const int C2 = C1 + 5;
 
-        public SampleClass(int p1, int p2)
+            public SampleClass(int p1) : this(p1, "Bob")
         {
             x = p1;
-            y = p2;
+           
         }
+        public SampleClass(string v): this(12, v)
+            {
+                v = "Alan";
+               
+            }
+        public SampleClass(int p1, string v)
+            {
+                p1 = 12;
+                v = "Name";
+            }
+
 
     }
 
     static void Main()
     {
-        var mC = new SampleClass(11, 22);
+        var mC = new SampleClass(11, "Name");
         Console.WriteLine($"x = {mC.x}, y = {mC.y}");
         Console.WriteLine($"C1 = {SampleClass.C1}, C2 = {SampleClass.C2}");
+        Console.ReadLine();
     }
 }
 }
