66bee5824e03a42f34b756cc40cbc1ab3d1105a3
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-08-12 12:03:34 -0500

    update

---
+++ DatabaseApp/DatabaseApp/SchoolContextDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace DatabaseApp
+{
+    public class SchoolContextDbContext: DbContext
+    {
+        public SchoolContextDbContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
+
